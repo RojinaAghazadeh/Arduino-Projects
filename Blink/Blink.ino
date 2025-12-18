19645d93a5739be4f56376929554e7944263e25d
Rojina <rozhina.agh@gmail.com>
2025-12-18 16:17:50 +0330

    Delete Blink directory

--- Blink/Blink.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  pinMode(13, OUTPUT);  // Enabling pin 13 for the output
-}
-void loop() {
-  digitalWrite(13, HIGH);  // on
-  delay(1000);             // 1 second
-  digitalWrite(13, LOW);   // off
-  delay(1000);             // 1 second
-}
